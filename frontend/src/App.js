3b9986f219a38e86ac9a75eef4517f438ad9ede8
erenonur19 <eok@eskisehir.edu.tr>
2022-08-23 23:08:51 +0300

    login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,34 @@
 import './App.css';
 import Login from './components/Login';
-import Register from './components/Register';
-import { useState } from 'react';
+import logo from './img/logo.png'
+
+
 function App() {
 
   return (
-    <div >
+    <div>
+      <div style={{position:'absolute', top:20,left:20}}>
+        <img alt='logo' src={logo} style={{width:'120px'}}/>
+      </div>
       <Login></Login>
+      
+      <div style={{position:'absolute', bottom:10,left:20}}>
+        <div className='footer'>
+        <ul className='footer-nav'>
+          <a href='/'>Kullanıcı Anlaşması</a>
+          <a href='/'>Gizlilik Politikası</a>
+          <a href='/'>Topluluk Yönergeleri</a>
+          <a href='/'>Çerez Politikası</a>
+          <a href='/'>Telif Hakkı Politikası</a>
+          <a href='/'>Geri Bildirim Gönder</a>
+          <a href='/'>Dil
+          <svg viewBox="0 0 20 14" width="15" height="10" preserveAspectRatio="xMinYMin meet" xmlns="http://www.w3.org/2000/svg"><path d="M8 9l5.93-4L15 6.54l-6.15 4.2a1.5 1.5 0 01-1.69 0L1 6.54 2.07 5z" fill="currentColor"></path></svg> 
+          </a>
+          
+        </ul>
+        </div>
+        
+      </div>
     </div>
   );
 }
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -3,12 +3,18 @@ import React from 'react'
 function Login() {
   return (
     <div className='form'>
-        <h1>Login to your account</h1>
+        <h1>Oturum Aç</h1>
+        <p>Profesyonel dünyanızla ilgili güncel haberlere sahip olun</p>
         
-        <input placeholder="Username"></input>
+        <input placeholder="E-posta veya Telefon"></input>
         
-        <input placeholder="Password"></input>
-        <button>Log In</button>
+        <input placeholder="Şifre"></input>
+        <a href='/'>Şifrenizi mi unuttunuz? </a>
+        <button className='btn'>Oturum açın</button>
+        
+        <div className='or'>Üye değil misiniz? <a href='/'>Hemen Katılın</a></div>
+        {/* <button className='btn' style={{backgroundColor:'#fff',color:'#0073b1', border:'1px solid black'}}>Üye Ol</button> */}
+       
     </div>
   )
 }
